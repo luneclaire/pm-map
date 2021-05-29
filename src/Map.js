4fa2d6d6b66a6d7b9b835d2c93fae95e2f3da31a
luneclaire <minjeon@minjiyeon-ui-MacBookAir.local>
2021-05-29 17:19:45 +0900

    Add basic zoom in and zoom out function

--- src/Map.js
+++ src/Map.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import ReactMapGL, { Layer, Source } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import data from './HangJeongDong_ver20210401';
+import data from './Sido';
+import { Button } from 'antd';
+import { ZoomOutOutlined } from '@ant-design/icons';
 
 export function Map() {
   const MAP_TOKEN = 'pk.eyJ1IjoibHVuZWNsYWlyZSIsImEiOiJja3A2dzRkYnAwMDJtMnBwYW1pbHV2aXN1In0.XDowr_anEYxEmHwwFqqVyA';
@@ -13,6 +15,24 @@ export function Map() {
     height: '600px',
     zoom: 5.9
     });
+
+  const zoomToClicked = useCallback(event => {
+    setViewport({
+      ...viewport,
+      longitude: event.lngLat[0],
+      latitude: event.lngLat[1],
+      zoom: 7,
+    });
+  }, []);
+
+  const zoomOut = () => {
+    setViewport({
+      ...viewport,
+      latitude: 35.905546,
+      longitude: 127.935763,
+      zoom: 5.9,
+    });
+  }
   
   return (
     <div>
@@ -20,6 +40,7 @@ export function Map() {
         {...viewport}
         mapStyle="mapbox://styles/mapbox/light-v9"
         mapboxApiAccessToken={MAP_TOKEN}
+        onClick={zoomToClicked}
       >
         <Source
           id="korea"
@@ -33,6 +54,12 @@ export function Map() {
           paint={{"fill-color": "#b83132", "fill-opacity": 0.4}}
         />
       </ReactMapGL>
+      <Button
+        type="primary"
+        shape="circle"
+        icon={<ZoomOutOutlined/>}
+        onClick={zoomOut}
+      />
     </div>
   );
 }
