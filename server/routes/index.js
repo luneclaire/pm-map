11c1f9c81e7e3866ed8e62917985c2a930568af0
seungyeon119 <ssy01191@gmail.com>
2021-05-31 03:32:42 +0900

    Connect DB

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,30 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const Sido = require("../models/sido");
+const { GetsidoAirdata } = require("./getSido");
 
-const { getSido } = require("./getSido");
-
-const sidoAirdata = (callback) => {
-    const sidoNames = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주', '세종'];
-    var result = []
-    var cnt = 0;
-    for (const sidoName of sidoNames){ // 모든 시도에 대해서 반복적으로 api 호출
-        getSido(sidoName, (err, {air}={})=>{
-            if(err){
-                console.log('에러발생!');
-            }
-            cnt++;
-            result.push(air);
-            if (cnt === sidoNames.length){
-                callback({
-                    result:result
-                })
-            }
-        });
-    }    
+// 숫자 formatting (두 자리 맞추기)
+const leadingZero = (data) => { 
+    return ("0"+data).slice(-2);
 }
+
+// DB에서 현재(가장 최신의) 미세먼지/초미세먼지 수치 가져오기
+const findSido = (year, month, date, hour, callback) => {
+    var dateTime = '';
+
+    if (hour === 0){ // n일 00시 -> (n-1)일 24시로 변환 (이유: 미세먼지 api에서는 0시를 24시로 표기)
+        dateTime = year+"-"+month+"-"+leadingZero(date-1)+" 24:00"; // 아직 해결 못한것 : date=0되면..?
+    }else{
+        dateTime = year+"-"+month+"-"+leadingZero(date)+" "+leadingZero(hour)+":00";
+    }
+
+    // DB에서 dateTime이 현재 시각과 가장 가까운 데이터 가져오기
+    Sido.find({dateTime: dateTime}, function(error,sido){
+        if(error){
+            console.log(error);
+        }else{
+            if(sido.length === 17){
+                callback({
+                    sido:sido
+                });
+            }else{ // 최신 데이터로 업데이트
+                GetsidoAirdata(sido.length===0);
+                console.log('updated');
+                findSido(year, month, date, hour, callback);
+            }
+        }
+    });
+}
+                    
 router.get('/', function(req, res, next){
-    sidoAirdata((result)=>{
-        res.send(result)
+
+    // dateTime 가져오기
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = leadingZero(today.getMonth()+1);
+    var date = today.getDate();
+    var hour = today.getHours();
+
+    // DB에서 현재 미세먼지/초미세먼지 수치 가져오기
+    findSido(year, month, date, hour, (sido)=>{
+        res.send(sido);
     });
 });
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,36 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const schedule = require('node-schedule');
+const { GetsidoAirdata } = require("./routes/getSido");
+
+const fs = require('fs')
+var text = fs.readFileSync("./server/dbconnection.txt").toString('utf-8');
+
 const app = express()
 const port = 3001
+
+// mongodb atlas 연결
+mongoose.connect(text, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+});
+
+var connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', function callback () {
+	connection.db.listCollections().toArray(function (err, names) {
+    if (err) {
+      console.log(err);
+    } else {
+      GetsidoAirdata((names.length === 0)); 
+      console.log("updated");
+    }
+  });
+});
+
+// xx시 10분에 DB업데이트 
+var j = schedule.scheduleJob('0 10 * * * *', function(){ 
+  GetsidoAirdata(false);
+});
 
 const api = require('./routes/index');
 app.use('/api', api);
