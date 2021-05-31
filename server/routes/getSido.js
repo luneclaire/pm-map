a3cfbbc46101eb7c00968c26fea65fb5a81cb081
seungyeon119 <ssy01191@gmail.com>
2021-06-01 01:46:51 +0900

    Refactoring

--- server/routes/getSido.js
+++ server/routes/getSido.js
@@ -1,84 +1,88 @@
-var request = require('request');
 const Sido = require("../models/sido");
+const axios = require('axios');
 
 // const serviceKey = 'Vtgkpa6WDF3%2BrOl7MToep50Jv3ahvFmqv6fcyko7soqyfZTFQTAFCQOiSK7Is0Wud7kLs4WyEzTcRTl3Esbxbg%3D%3D';
 const serviceKey = 'JzAjCMSkJKezoT9lpf%2FilQVb5808SC4cc7FU83dGJdO939K0UWHTn%2Bj2J6l%2FaxyCityrbAoQLJIV3w8x2hdqmQ%3D%3D';
+// const serviceKey = 'dC0Mal22V6WU0%2BFhs1pxRYGtxCk3gyIU84PpYDzSQJgl1A86QtlR5iPgjNHnNMPjEn55t7YbHljqayKmwclVlg%3D%3Ds';
 
-const getSido = (sidoName, callback) => {
+async function requestApi(sidoName){
     const url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?';
-
     var queryParams = encodeURIComponent('ServiceKey') + '=' + serviceKey;
     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100');
     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1');
     queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.3'); //버젼
     queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent(sidoName); //시도 이름
     queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); //josn으로 받기
-
-    request({
-        url: url + queryParams,
-        method: 'GET',
-    }, function (err, res, body) {
-
-        const airs = JSON.parse(body).response.body.items;
-        var pm10Sum = 0;
-        var pm25Sum = 0;
-        var sidoLength = airs.length;
-        const dateTime = airs[0].dataTime;
-
-        for (const air of airs){ // 도(특별시)별 모든 측정장소의 pm10/25Value 평균값 구하기
-            pm10Value = air.pm10Value;
-            pm25Value = air.pm25Value;
-
-            if ((pm10Value === '-' ) | (pm10Value === null )) {  // 미세먼지 null값 처리
-                sidoLength -= 1;
-                continue
-            }
-            if ((pm25Value === '-' ) | (pm25Value === null )) {  // 초미세먼지 null값 처리
-                sidoLength -= 1;
-                continue
-            }
-            pm10Sum += Number(pm10Value);
-            pm25Sum += Number(pm25Value);
-        }
-
-        callback(undefined, {
-            air:{
-                sidoName: sidoName, // 시도 이름
-                dateTime: dateTime, // 측정시간
-                pm10Value: Math.round(pm10Sum/sidoLength), // 해당 지역의 평균 미세먼지 수치
-                pm25Value: Math.round(pm25Sum/sidoLength), // 해당 지역의 평균 초미세먼지 수치
-            }
-        });
-    });
-};
-
-
-const GetsidoAirdata = (isNew) => {
-    const sidoNames = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주', '세종'];
     
+    const uri = url + queryParams;
+    var {data} = await axios.get(uri);
+    data = data.response.body.items;
+
+    return data
+}
+
+async function getSidoData(sidoName){
+    const airs = await requestApi(sidoName);
+    var pm10Sum = 0;
+    var pm25Sum = 0;
+    var sidoLength = airs.length;
+    const dateTime = airs[0].dataTime;
+
+    for (const air of airs){ // 도(특별시)별 모든 측정장소의 pm10/25Value 평균값 구하기
+        pm10Value = air.pm10Value;
+        pm25Value = air.pm25Value;
+
+        if ((pm10Value === '-' ) | (pm10Value === null )) {  // 미세먼지 null값 처리
+            sidoLength -= 1;
+            continue
+        }
+        if ((pm25Value === '-' ) | (pm25Value === null )) {  // 초미세먼지 null값 처리
+            sidoLength -= 1;
+            continue
+        }
+        pm10Sum += Number(pm10Value);
+        pm25Sum += Number(pm25Value);
+    }
+
+    const data = {
+        sidoName: sidoName, // 시도 이름
+        dateTime: dateTime, // 측정시간
+        pm10Value: Math.round(pm10Sum/sidoLength), // 해당 지역의 평균 미세먼지 수치
+        pm25Value: Math.round(pm25Sum/sidoLength), // 해당 지역의 평균 초미세먼지 수치
+    }
+    return data
+}
+
+
+function updateDB(data){
+    const query = {
+        "sidoName": data.sidoName,
+        "dateTime": data.dateTime
+    };
+    const update = {
+        "$set":{"pm10Value": data.pm10Value,
+                "pm25Value": data.pm25Value}
+    };
+    const options = { "upsert": true };
+
+    Sido.updateOne(query, update, options)
+    .then(()=>console.log(data.sidoName, 'upserted!')) 
+}
+
+
+async function getAllSidoData(){
+    const sidoNames = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주', '세종'];
     for (const sidoName of sidoNames){ // 모든 시도에 대해서 반복적으로 api 호출
-        getSido(sidoName, (err, {air}={})=>{
-            if(err){
-                console.log('에러발생!');
-            }
-            if(isNew){
-                const sido = new Sido(air);
-                sido.save();
-            }else{
-                Sido.updateOne({
-                    sidoName: air.sidoName,
-                    dateTime: air.dateTime},
-                    {
-                        $set:{
-                            pm10Value: air.pm10Value,
-                            pm25Value: air.pm25Value
-                        }
-                    },
-                    {upsert:true} 
-                );
-            }
-        });
-    }    
-  };
-  
-exports.GetsidoAirdata = GetsidoAirdata;
+        const data = await getSidoData(sidoName);
+        const hour = Number(new Date().getHours()); // 현재 시간
+        const apiHour = Number((data.dateTime).slice(-5,-3)); // 측정 시간
+        
+        if(apiHour < hour){ // api에서 아직 업데이트 되지 않음
+            continue
+        }else{
+            updateDB(data); // 해당 지역 수치 DB 업데이트
+        }
+    }
+}
+
+exports.getAllSidoData = getAllSidoData;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,52 +1,69 @@
 var express = require('express');
 var router = express.Router();
+
 const Sido = require("../models/sido");
-const { GetsidoAirdata } = require("./getSido");
+const { getAllSidoData } = require("./getSido");
+
+// const fs = require('fs');
+// const regionData = require("./region.json")
+// const { getStationList } = require("./getRegion");
 
 // 숫자 formatting (두 자리 맞추기)
-const leadingZero = (data) => { 
+function leadingZero (data){
     return ("0"+data).slice(-2);
 }
 
-// DB에서 현재(가장 최신의) 미세먼지/초미세먼지 수치 가져오기
-const findSido = (year, month, date, hour, callback) => {
-    var dateTime = '';
-
+function getDateTime(today, year, month, date, hour){
     if (hour === 0){ // n일 00시 -> (n-1)일 24시로 변환 (이유: 미세먼지 api에서는 0시를 24시로 표기)
-        dateTime = year+"-"+month+"-"+leadingZero(date-1)+" 24:00"; // 아직 해결 못한것 : date=0되면..?
-    }else{
-        dateTime = year+"-"+month+"-"+leadingZero(date)+" "+leadingZero(hour)+":00";
-    }
-
-    // DB에서 dateTime이 현재 시각과 가장 가까운 데이터 가져오기
-    Sido.find({"dateTime": dateTime}, function(error,sido){
-        if(error){
-            console.log(error);
-        }else{
-            if(sido.length === 17){
-                callback({
-                    sido:sido
-                });
-            }else{ // 1시간 전 데이터 호출
-                findSido(year, month, date, hour-1, callback);
-            }
+        var yesterday = new Date(today.setDate(today.getDate() - 1)); //어제 날짜 가져오기
+        yesterdayDate = yesterday.getDate(); 
+        hour = 24;
+        if (Number(date) === 1){ // 만약 오늘이 n월 1일이면 전날은 (n-1)월 
+            month = yesterday.getMonth()+1;
         }
+        date = yesterdayDate;
+    }    
+
+    dateTime = year+"-"+leadingZero(month)+"-"+leadingZero(date)+" "+leadingZero(hour)+":00";
+    return dateTime;
+}
+
+// DB에서 현재(가장 최신의) 미세먼지/초미세먼지 수치 가져오기
+async function findSido(today, year, month, date, hour, callback){
+    const dateTime = getDateTime(today, year, month, date, hour);
+    Sido.find({"dateTime": dateTime}).exec()
+    .then((result) =>{
+        if(result.length > 10){ 
+            callback({
+                result:result
+            });
+        }else{
+            getAllSidoData()
+            .then(findSido(today, year, month, date, hour-1, callback)); // 1시간 전 데이터 호출
+        } 
+    })
+    .catch((err)=>{
+        console.error(err);
     });
 }
-                    
-router.get('/', function(req, res, next){
 
+router.get('/', async function(req, res, next){
+    
     // dateTime 가져오기
     var today = new Date();
     var year = today.getFullYear();
-    var month = leadingZero(today.getMonth()+1);
+    var month = today.getMonth()+1;
     var date = today.getDate();
     var hour = today.getHours();
 
-    // DB에서 현재 미세먼지/초미세먼지 수치 가져오기
-    findSido(year, month, date, hour, (sido)=>{
-        res.send(sido);
-    });
+    findSido(today, year, month, date, hour, (result)=>{
+        res.send(result);
+    })
+
+    // 시군구별 측정소 검색
+    // let result = await getStationList(regionData);
+    // let data = JSON.stringify(result);
+    // fs.writeFileSync('matchRegion_Station.json', data);
 });
 
 module.exports = router;
