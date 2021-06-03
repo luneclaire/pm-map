1bc9385e58cd995a050a961994ab6fc662e98b70
luneclaire <minjeon@minjiyeon-ui-MacBookAir.local>
2021-06-03 19:03:28 +0900

    Add color legend

--- src/Map.js
+++ src/Map.js
@@ -4,8 +4,35 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import sidoGeoJson from './Sido';
 import sigunguGeoJson from './Sigungu';
 import { Button } from 'antd';
-import { ZoomOutOutlined } from '@ant-design/icons';
+import { SmileTwoTone, MehTwoTone, FrownTwoTone, AlertTwoTone, CloseCircleTwoTone, ZoomOutOutlined } from '@ant-design/icons';
 import bbox from '@turf/bbox'
+
+function ColorLegend() {
+  return (
+    <ul className="color-legend">
+      <li>
+        <SmileTwoTone className="align-left" twoToneColor = "#1C3FFD"/>
+        <span className="alight-right">좋음 ~30</span>
+      </li>
+      <li>
+        <MehTwoTone className="align-left" twoToneColor = "#87ae22"/>
+        <span className="alight-right"/>보통 ~80
+      </li>
+      <li>
+        <FrownTwoTone className="align-left" twoToneColor = "#FFD10F"/>
+        <span className="alight-right"/> 나쁨 ~150
+      </li>
+      <li>
+        <AlertTwoTone className="align-left" twoToneColor = "#D90000"/>
+        <span className="alight-right"/>&nbsp;&nbsp;&nbsp;최악 151~
+      </li>
+      <li>
+        <CloseCircleTwoTone className="align-left" twoToneColor = "#565656"/>
+        <span className="align-right">정보 없음</span>
+      </li>
+    </ul>
+  );
+}
 
 export function Map( {pmSwitch, changeAddr, SidoDB, SigunguDB} ) {
   const MAP_TOKEN = 'pk.eyJ1IjoibHVuZWNsYWlyZSIsImEiOiJja3A2dzRkYnAwMDJtMnBwYW1pbHV2aXN1In0.XDowr_anEYxEmHwwFqqVyA';
@@ -175,6 +202,7 @@ export function Map( {pmSwitch, changeAddr, SidoDB, SigunguDB} ) {
         icon={<ZoomOutOutlined/>}
         onClick={zoomOut}
       />
+      <ColorLegend/>
     </div>
   );
 }
