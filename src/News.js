4cebb814f2827e00a2e87c63420dd2a14da94815
SSZ4 <37368388+SSZ4@users.noreply.github.com>
2021-06-06 16:24:00 +0900

    Add news Date and Delete redundant string in news description
    뉴스에 뉴스 날짜를 더했고, 뉴스 description에서 [사진 출처 : xx뉴스] 홍길동 같은 부분이 나오는 기사가 보여서 사진출처 부분을 삭제했습니다.

--- src/News.js
+++ src/News.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 export function News(){
     const [newsList, setNewsList] = useState([{
@@ -36,31 +37,43 @@ export function News(){
     }
     getNews()
   }, []);
-  
+  //<b>는 검색어 query인 미세먼지에만 붙어있는데, 필요없어서 제거
   return(
     <div>
         <div>
             <a className="newsarea" href={newsList[0].originallink} target="_blank" rel="noopener noreferrer">
-              {newsList[0].title.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '')}
+              {newsList[0].title.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '\"')}
             </a>
-            <p>
-               {newsList[0].description.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '')}
+            <p className="descriptionarea">
+              {newsList[0].description.slice(0,newsList[0].description.indexOf('[사진')).replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '\"')}
+              <br></br>
+              <div className="pubdatearea">
+                {newsList[0].pubDate.slice(0,16)}
+              </div>
             </p>
         </div>
         <div>
             <a className="newsarea" href={newsList[1].originallink} target="_blank" rel="noopener noreferrer">
-              {newsList[1].title.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '')}
+              {newsList[1].title.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '\"')}
             </a>
-            <p>
-               {newsList[1].description.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '')}
+            <p className="descriptionarea">
+              {newsList[1].description.slice(0,newsList[1].description.indexOf('[사진')).replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '\"')}
+              <br></br>
+              <div className="pubdatearea">
+                {newsList[1].pubDate.slice(0,16)}
+              </div>
             </p>
         </div>
         <div>
             <a className="newsarea" href={newsList[2].originallink} target="_blank" rel="noopener noreferrer">
-              {newsList[2].title.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '')}
+              {newsList[2].title.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '\"')}
             </a>
-            <p>
-               {newsList[2].description.replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '')}
+            <p className="descriptionarea">
+              {newsList[2].description.slice(0,newsList[2].description.indexOf('[사진')).replaceAll('<b>', '').replaceAll('</b>', '').replaceAll('&quot;', '\"')}
+              <br></br>
+              <div className="pubdatearea">
+                {newsList[2].pubDate.slice(0,16)}
+              </div>
             </p>
         </div>
     </div>
